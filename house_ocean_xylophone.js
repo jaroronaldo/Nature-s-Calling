3af55e0b3db71885c8385c5ea5d3af2ecc886234
jaroronaldo <136631598+jaroronaldo@users.noreply.github.com>
2023-07-19 14:48:50 +0700

    Fix broken tests

---
+++ house_ocean_xylophone.js
@@ -0,0 +1,115 @@
+//1
+
+// Create the title of the poem
+let poemTitle = "Nature's Calling";
+
+//2
+
+// Create a variable to store the poem
+let poem = "";
+
+//3
+
+// Create the first line of the poem
+poem = poem + "I'm listening to the trees \n";
+
+//4
+
+// Create the second line of the poem
+poem = poem + "Quietly rustling in the breeze \n";
+
+//5
+
+// Create the third line of the poem
+poem = poem + "Their whispers are so soothing \n";
+
+//6
+
+// Create the fourth line of the poem
+poem = poem + "The sound of nature is enthralling \n";
+
+//7
+
+// Create the fifth line of the poem
+poem = poem + "A tranquil atmosphere to behold \n";
+
+//8
+
+// Create the sixth line of the poem
+poem = poem + "The land as if stories told \n";
+
+//9
+
+// Create the seventh line of the poem
+poem = poem + "The birds chirping sweetly in the sky \n";
+
+//10
+
+// Create the eighth line of the poem
+poem = poem + "Singing like a lullaby \n";
+
+//11
+
+// Create the ninth line of the poem
+poem = poem + "The tinkling of a stream \n";
+
+//12
+
+// Create the tenth line of the poem
+poem = poem + "A nature lover's dream \n";
+
+//13
+
+// Create the eleventh line of the poem
+poem = poem + "The vast horizon of blue \n";
+
+//14
+
+// Create the twelfth line of the poem
+poem = poem + "A sight that will renew \n";
+
+//15
+
+// Create the thirteenth line of the poem
+poem = poem + "The roar of a lion as I pass his way \n";
+
+//16
+
+// Create the fourteenth line of the poem
+poem = poem + "Majestic and proud to stay \n";
+
+//17
+
+// Create the fifteenth line of the poem
+poem = poem + "Nature is alive, moving and growing \n";
+
+//18
+
+// Create the sixteenth line of the poem
+poem = poem + "Allow yourself to keep flowing \n";
+
+//19
+
+// Create the seventeenth line of the poem
+poem = poem + "The world is incredible and full of awe \n";
+
+//20
+
+// Create the eighteenth line of the poem
+poem = poem + "The beauty of nature will never be outdone \n";
+
+//21
+
+// Create the nineteenth line of the poem
+poem = poem + "Let us all run out to explore \n";
+
+//22
+
+// Create the twentieth line of the poem
+poem = poem + "And let nature's call be heard evermore! \n";
+
+//23
+
+// Print out the poem with the title
+console.log(poemTitle);
+console.log(poem);
